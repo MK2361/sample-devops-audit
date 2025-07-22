b13e756b7bf60cf348af4aed0f01c0dd33605df9
MK2361 <uhho_xyz@yahoo.co.jp>
2025-07-23 07:02:33 +0900

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,8 @@
+import os, sys
+
+cmd = sys.argv[1]          # ← ここが外部入力
+os.system(cmd)             # ← Data flow あり＝アラート
+
 def hello():
     print("Hello Secure DevOps!")
 if __name__ == "__main__":
