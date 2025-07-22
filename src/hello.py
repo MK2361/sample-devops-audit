99db02ac9a31769b898726610058366effd92692
MK2361 <uhho_xyz@yahoo.co.jp>
2025-07-23 07:10:09 +0900

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,3 +1,8 @@
+import subprocess, sys
+
+cmd = sys.argv[1]                         # tainted source
+subprocess.run(cmd, shell=True)           # sink: subprocess + shell=True
+
 def hello():
     print("Hello Secure DevOps!")
 if __name__ == "__main__":
